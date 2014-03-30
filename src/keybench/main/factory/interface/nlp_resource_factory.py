09e075862c557ec7385a5aa9a93d02de9188c665
adrien-bougouin <adrien.bougouin@gmail.com>
2014-03-30 15:15:48 +0200

    move the tagset information from the resource factory to the pos tagger interface

--- src/keybench/main/factory/interface/nlp_resource_factory.py
+++ src/keybench/main/factory/interface/nlp_resource_factory.py
@@ -4,47 +4,14 @@ class KBNLPResourceFactoryI(object):
   """The configuration of the NLP resources to use for each language.
 
   The abstract factory providing the usefull Natural Language Processing
-  resources (e.g. list of stop words, Part-of-Speech tagset) for specific
-  languages.
+  resources (e.g. list of stop words) for specific languages.
   """
-
-  class POSTagKey:
-    """Part-of-Speech names.
-
-    Part-of-Speech keys to use with the C{dict} returned by
-    C{KBNLPResourceFactory.posTags()}.
-
-    Attributes:
-      NOUN_KEY: Key used for noun tags.
-      ADJECTIVE_KEY: Key used for adjective tags.
-      VERB_KEY: Key used for verb tags.
-      ADVERB_KEY: Key used for adverb tags.
-      PREPOSITION_KEY: Key used for preposition tags.
-      DETERMINER_KEY: Key used for determiner tags.
-    """
-
-    NOUN_KEY        = "__noun__"
-    ADJECTIVE_KEY   = "__adjective__"
-    VERB_KEY        = "__verb__"
-    ADVERB_KEY      = "__adverb__"
-    PREPOSITION_KEY = "__preposition__"
-    DETERMINER_KEY  = "__determiner__"
 
   def stopLists(self):
     """Provides a list of stop words.
 
     Returns:
       The C{list} of C{string} words to use as stop words.
-    """
-
-    raise exception.KBConfigurationException(self, "Uncomplete NLP resource configuration!")
-
-  def posTags(self):
-    """Provides the tagset to use.
-
-    Returns:
-      The C{dict} of tags (C{list} of C{string} tags) associated to a
-      grammatical class (see C{KBNLPResourceFactoryI.POSTagKey}).
     """
 
     raise exception.KBConfigurationException(self, "Uncomplete NLP resource configuration!")
--- src/keybench/main/nlp_tool/interface/pos_tagger.py
+++ src/keybench/main/nlp_tool/interface/pos_tagger.py
@@ -2,7 +2,44 @@ import exceptions
 
 class KBPOSTaggerI(object):
   """Interface of a tokenized sentence POS tagger.
+
+  Attributes:
+    tagset: The C{dict} of tags (C{list} of C{string} tags) associated to a
+      grammatical class (see C{KBPOSTaggerI.POSTagKey}).
   """
+
+  ##############################################################################
+  class POSTagKey:
+    """Part-of-Speech names.
+
+    Part-of-Speech names to use as tagset keys. Those names are generic and
+    meant to match multiple tags.
+
+    Attributes:
+      NOUN_KEY: Key used for noun tags.
+      ADJECTIVE_KEY: Key used for adjective tags.
+      VERB_KEY: Key used for verb tags.
+      ADVERB_KEY: Key used for adverb tags.
+      PREPOSITION_KEY: Key used for preposition tags.
+      DETERMINER_KEY: Key used for determiner tags.
+    """
+
+    NOUN_KEY        = "__noun__"
+    ADJECTIVE_KEY   = "__adjective__"
+    VERB_KEY        = "__verb__"
+    ADVERB_KEY      = "__adverb__"
+    PREPOSITION_KEY = "__preposition__"
+    DETERMINER_KEY  = "__determiner__"
+  ##############################################################################
+
+  def __init__(self):
+    super(KBPOSTaggerI, self).__init__()
+
+    self._tagset = {}
+
+  @property
+  def tagset(self):
+    return self._tagset
 
   def tag(self, tokenized_sentences):
     """POS tags tokenized sentences.
